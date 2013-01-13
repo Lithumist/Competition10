a9c18f56dd9b1bc38351e653b34c3de657ed3716
Pedro Custodio <magneticplum@live.co.uk>
2013-01-13 10:40:56 +0000

    +More Setup

---
+++ C10Engine/main.cpp
@@ -0,0 +1,18 @@
+/*
+	
+	Pedro Custodio 2013
+
+
+	Unnamed game for the 10th Moosader Boards competition
+
+*/
+
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	system("pause");
+	return 0;
+}
